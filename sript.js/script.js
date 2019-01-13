4ce24c9be7a5a7d0a728fa43ac55962551929f75
Gennadii Chepiha <stewart1396@gmail.com>
2019-01-13 16:38:22 +0200

    third commit

--- sript.js/script.js
+++ sript.js/script.js
@@ -19,15 +19,22 @@ toTop.addEventListener('click', function(){
 var button = document.getElementById('button');
 var popUp = document.getElementById('popup');
 var close = document.getElementById('close');
-var backPop = document.getElementById('back')
+var backPop = document.getElementById('back');
+var closethere = document.getElementById('closethere');
 
     button.addEventListener('click', function(){
         popUp.style.display = "block";
         backPop.style.background = "rgba(0,0,0,0.4)";
-        backPop.style.position = "fixed"	;	
+        backPop.style.position = "fixed";	
     });
     
     close.addEventListener('click', function() {
+        popUp.style.display = "none";
+        backPop.style.background = "rgba(0,0,0,0.0)";
+        backPop.style.position = "relative";		
+    });
+
+    closethere.addEventListener('click', function() {
         popUp.style.display = "none";
         backPop.style.background = "rgba(0,0,0,0.0)";
         backPop.style.position = "relative";		
